bfb854339d058590b199800e7889309e72bbade3
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2020-11-19 22:57:24 +0100

    Update datasource_ad-user-generate-table-managers.ps1

--- Manual setup/datasource_ad-user-generate-table-managers.ps1
+++ Manual setup/datasource_ad-user-generate-table-managers.ps1
@@ -22,7 +22,7 @@ try {
       
     if($allUsersCount -gt 0){
         foreach($user in $allUsers){
-            $adUser = Get-ADUser $user -properties *
+            $adUser = Get-ADUser $user -properties SamAccountName,displayName,UserPrincipalName,Description,Department,Title,DistinguishedName
              
             $returnObject = @{
                 SamAccountName = $adUser.SamAccountName;
