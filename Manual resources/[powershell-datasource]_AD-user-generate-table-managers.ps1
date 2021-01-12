b23aa648913760aced0eb730a492b09e96c46508
Michiel85 <68066270+Michiel85@users.noreply.github.com>
2021-01-12 13:55:38 +0100

    Updated file with powershell data source

---
+++ Manual resources/[powershell-datasource]_AD-user-generate-table-managers.ps1
@@ -0,0 +1,42 @@
+try {
+    $userUPN = $datasource.selectedUser.UserPrincipalName
+    
+    if([String]::IsNullOrEmpty($userUPN) -eq $false){
+        $selectedUser = Get-ADuser -Filter { UserPrincipalName -eq $userUPN } -Properties manager
+        $currentManager = $selectedUser.Manager
+    }
+    
+    $groups = $ADmanagerGroups | ConvertFrom-Json
+    Write-information "Getting managers from AD groups $ADmanagerGroups"
+    
+    $allUsers = foreach($group in $groups) {
+        Get-ADGroupMember -identity $group.name | Where objectClass -eq "user"
+    }
+
+    $allUsersCount = @($allUsers).Count
+    $allUsers = $allUsers | Sort-Object -Property name    
+    Write-information "Result count: $allUsersCount"
+     
+    if($allUsersCount -gt 0){
+        foreach($user in $allUsers){
+            $adUser = Get-ADUser $user -properties SamAccountName, displayName, UserPrincipalName, Description, Department, Title, DistinguishedName
+            
+            $returnObject = @{
+                SamAccountName = $adUser.SamAccountName;
+                displayName = $adUser.displayName;
+                UserPrincipalName = $adUser.UserPrincipalName;
+                Description  =$adUser.Description;
+                Department = $adUser.Department;
+                Title = $adUser.Title;
+                selected = ($adUser.DistinguishedName -eq $currentManager)
+            }
+            
+            Write-output $returnObject
+        }
+    } else {
+        return
+    }
+} catch {
+    Write-error "Error getting members for AD group [$groupName]. Error: $($_.Exception.Message)"    
+    return
+}
